7846e49d179ad4964d994325f1516f679301fcc5
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-05-30 00:01:32 -0400

    JavaScript Review

--- src/components/reviseES6/day1.js
+++ src/components/reviseES6/day1.js
@@ -47,11 +47,74 @@ function sayHi(){
 
 /*
 let scope is defined by the nearest enclosing block
+var scope ia defined as global
 
+function a(){
+    'use strict';
+    for (let x =1; x<10; x++){
+        console.log(x);
+    }
+    console.log(x);
+}
 
+let y = 'global';
+console.log(this.y);
+// console.log(window.y);
+a(); 
+
+const MY_OBJECT = {"name": "Dickyi"};
+// MY_OBJECT = {"p":"A"}; This fails!
+Object.freeze(MY_OBJECT);
+console.log("Hello?")
+console.log(MY_OBJECT.name)
 */
 
+function findMax() {
+    var i;
+    var max = -Infinity;
+    for (i = 0; i<arguments.length; i++) {
+        if (arguments[i]>max) {
+            max = arguments[i];
+        }
+    }
+    return max;
+}
 
+var x = findMax(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// console.log(x)
+
+// In JavaScript, functions are objects. Therefore, there is no overloading
+function log(x=10, y =5){
+    console.log(x + "Arguments" + y);
+}
+
+var multiply = (num1, num2) => num1 * num2;
+// console.log(multiply(5, 5))
+
+const future = ['Temka', 'Dickyi']
+const goodLife = ['home', 'happiness', 'nonstop', ...future]
+console.log(goodLife)
+
+function sum(x,y,...more) {
+    var total = x + y;
+    if (more.length){
+        more.forEach((extra)=>total+=extra)
+    }
+    console.log(total)
+}
+// sum(5, 5, 5, 5, 5)
+
+// For in is used for objects.
+// For of is used for arrays.
+// var things = {'a':97, 'b':2, 'c':3};
+// for (let i in things){
+//     console.log(i+'  '+things[i])
+// }
+
+var things = [1, 'a', 3];
+for (let i of things){
+    console.log(i)
+}
 
 
 
